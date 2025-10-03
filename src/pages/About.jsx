70e3eeb107af681b30c81758b362e28d7c903c39
reyandru <bariatareiandrew@gmail.com>
2025-10-03 19:16:40 +0800

    about page animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Sidebar from '../reusable/Sidebar';
+import gsap from 'gsap';
 import '../css/hasSideBar.css';
 import '../css/about.css';
 import { onebyone, resume } from '../assets/assets';
 
 function About() {
   const [isCollapse, setIsCollapse] = useState(false);
+  const [age, setAge] = useState(null);
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem('theme');
     return savedTheme === 'dark' ? 'dark' : 'light';
+  });
+  const year = 2003;  
+  const month = 7;
+  const day = 19;
+  const textRef = useRef(null);
+  const picture = useRef(null);
+  const row1 = useRef(null);
+  const button = useRef(null);
+
+  function computeAge(){
+    const today = new Date();
+    let age = today.getFullYear() - year;
+
+    if(today.getMonth() < month || (today.getMonth() === month && today.getDate() < day)){
+      age--
+    }
+
+    setAge(age);
+  }
+
+  useEffect(() => {
+    computeAge();
   });
 
   useEffect(() => {
@@ -16,26 +40,70 @@ function About() {
     localStorage.setItem('theme', theme);
   }, [theme]);
 
+
+  useEffect(() => {
+  gsap.set(textRef.current, {opacity: 0 , x: -200, autoAlpha: 0});
+  gsap.set(picture.current, {opacity: 0});
+  gsap.set(row1.current, {y: 50, opacity: 0});
+  gsap.set(button.current, {opacity: 0});
+
+
+
+  const tl = gsap.timeline();
+
+    tl.to(textRef.current, {
+    x: 0,
+    opacity: 1,
+    autoAlpha: 1,
+    duration: 0.8,
+    ease: 'power3.out',
+  });
+
+   tl.to(picture.current,{
+    opacity: 1,
+    autoAlpha: 1,
+    duration: 0.5,
+    ease: 'power3.out',
+  });
+
+   tl.to(row1.current, {
+      opacity: 1,
+      y:0,
+      autoAlpha: 1,
+      duration: 0.6,
+      ease: 'power3.out',
+    });
+
+
+    tl.to(button.current,{
+    opacity: 1,
+    autoAlpha: 1,
+    duration: 0.3,
+    ease: 'power3.out',
+  });
+}, []);
+
+ 
   return (
     <>
       <Sidebar isCollapse={isCollapse} setIsCollapse={setIsCollapse} />
       <section className={`${isCollapse ? 'about-collapses' : 'about-container'}`}>
         <div className="section-head">
-          <h1 className="title" style={{ borderColor: theme === 'dark' ? 'white' : 'black' }}>About</h1>
-          <img src={onebyone} alt="" className="myImg" height={150} />
+          <h1 className="title" style={{ borderColor: theme === 'dark' ? 'white' : 'black' }} ref={textRef}>About</h1>
+          <img src={onebyone} alt="" className="myImg" height={150}  ref={picture}/>
         </div>
         <div className="about-wrapper">
           <div className="about-text">
-            <div className="about-info">
+            <div className="about-info" ref={row1}>
               <div className="about-column">
                 <div className="left">
-                  <p className="abt-name">Name: Rei Andrew C. Bariata</p>
+                  <p className="abt-name" >Name: Rei Andrew C. Bariata</p>
                   <p className="address">Address: P1 Brgy. Abulalas Hagonoy, Bulacan</p>
                   <p className="college">College: Bulacan State University (Hagonoy Campus)</p>
                 </div>
                 <div className="right">
-                  <p className="birthDay">Birthdate: August 19, <span id="year"></span></p>
-                  <p className="age">Age: <span id="ageNow"></span></p>
+                  <p className="birthDay">Birthdate: August 19, <span id="year">{year}</span></p>
+                  <p className="age">Age: <span id="ageNow">{age}</span></p>
                   <p className="course">Course: BSIT</p>
                   <p className="major">Major: Web and App Development</p>
                 </div>
@@ -50,7 +118,7 @@ function About() {
               </div>
             </div>
           </div>
-          <div className="resume-dl-btn">
+          <div className="resume-dl-btn" ref={button}>
             <button className="downloadResume">
               <a href={resume}>Download Resume</a>
             </button>
